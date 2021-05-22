f94063419c1b7226e6e65bc3d63d6997d5ad1352
Aryan <aryanmaurya1@outlook.com>
2021-05-22 15:40:29 +0530

    Added call to new functions for testing.

--- fatigu.go
+++ fatigu.go
@@ -6,27 +6,15 @@ import (
 	"time"
 )
 
-// type sample struct {
-// 	a int
-// }
-// func call (a sample) {
-// 	a.a = 100000
-// 	fmt.Println(a)
-// }
 func main() {
+
+	rand.Seed(time.Now().UnixNano())
+
 	// Arguments parsing and validation
 	var values Arguments
 	values = ParseArgs(values)
+	fmt.Println(values)
 	ValidateArgs(values)
 
-	fmt.Println(values)
-
-	rand.Seed(time.Now().UnixNano())
-	// var randomNuber int = rand.Int()
-
-	// var a sample
-	// fmt.Println(a)
-	// call(a)
-	// fmt.Println(a)
-
+	ReadConfigFile(values.configFilePath)
 }
