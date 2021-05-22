81a0ade22bff13c61394f639cedc5ef78b300b5c
Aryan <aryanmaurya1@outlook.com>
2021-05-22 20:33:55 +0530

    Used stringBuilder in place of raw string for arg table, added log-file arg support and updated checks.

--- args.go
+++ args.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"strings"
 )
 
@@ -16,6 +17,7 @@ type Arguments struct {
 	bodyFile       string // path to json file containing body
 	hits           int64  // number of concurrent hits to perform
 	configFilePath string // path to config file
+	logFile        string // file to write logs
 }
 
 func ParseArgs(values Arguments) Arguments {
@@ -25,48 +27,61 @@ func ParseArgs(values Arguments) Arguments {
 	flag.StringVar(&values.ep, "ep", "", "Endpoint to hit. [Base + Endpoint]")
 	flag.StringVar(&values.method, "method", "GET", "Comma separated list of methods to use.")
 	flag.StringVar(&values.body, "body", "", "Body to send in API call.")
-	flag.StringVar(&values.bodyFile, "bodyfile", "", "Path to json file to use as body content.")
-	flag.StringVar(&values.configFilePath, "file", "", "Path to config file. Required only in case of batch mode.")
+	flag.StringVar(&values.bodyFile, "body-file", "", "Path to json file to use as body content.")
+	flag.StringVar(&values.configFilePath, "config-file", "", "Path to config file. Required only in case of batch mode.")
 	flag.Int64Var(&values.hits, "hits", 1000, "Number of concurrent hits to perform.")
+	flag.StringVar(&values.logFile, "log-file", "", "File to write logs.")
 	flag.Parse()
 	return values
 }
 
 func (a Arguments) String() string {
-	var repr = ""
-	repr = repr + strings.Repeat("-", 56) + "\n"
-	repr = repr + fmt.Sprintf("| %-20s | %-30s|\n", "Flag", "Value")
-	repr = repr + strings.Repeat("-", 56) + "\n"
-	repr = repr + fmt.Sprintf("| %-20s | %-30v|\n", "Singleton Mode", a.s)
-	repr = repr + fmt.Sprintf("| %-20s | %-30v|\n", "Batch Mode", a.b)
-	repr = repr + fmt.Sprintf("| %-20s | %-30v|\n", "Base URL", a.base)
-	repr = repr + fmt.Sprintf("| %-20s | %-30v|\n", "Endpoint", a.ep)
-	repr = repr + fmt.Sprintf("| %-20s | %-30v|\n", "Methods", a.method)
-	repr = repr + fmt.Sprintf("| %-20s | %-30v|\n", "Body", a.body)
-	repr = repr + fmt.Sprintf("| %-20s | %-30v|\n", "Body File", a.bodyFile)
-	repr = repr + fmt.Sprintf("| %-20s | %-30v|\n", "Config File", a.configFilePath)
-	repr = repr + fmt.Sprintf("| %-20s | %-30v|\n", "Concurrent Hits", a.hits)
-	repr = repr + strings.Repeat("-", 56) + "\n"
+	var repr = strings.Builder{}
+	repr.Grow(1000)
+	repr.WriteString(strings.Repeat("-", 56) + "\n")
+	repr.WriteString(fmt.Sprintf("| %-20s | %-30s|\n", "Flag", "Value"))
+	repr.WriteString(strings.Repeat("-", 56) + "\n")
+	repr.WriteString(fmt.Sprintf("| %-20s | %-30v|\n", "Singleton Mode", a.s))
+	repr.WriteString(fmt.Sprintf("| %-20s | %-30v|\n", "Batch Mode", a.b))
+	repr.WriteString(fmt.Sprintf("| %-20s | %-30v|\n", "Base URL", a.base))
+	repr.WriteString(fmt.Sprintf("| %-20s | %-30v|\n", "Endpoint", a.ep))
+	repr.WriteString(fmt.Sprintf("| %-20s | %-30v|\n", "Methods", a.method))
+	repr.WriteString(fmt.Sprintf("| %-20s | %-30v|\n", "Body", a.body))
+	repr.WriteString(fmt.Sprintf("| %-20s | %-30v|\n", "Body File", a.bodyFile))
+	repr.WriteString(fmt.Sprintf("| %-20s | %-30v|\n", "Config File", a.configFilePath))
+	repr.WriteString(fmt.Sprintf("| %-20s | %-30v|\n", "Concurrent Hits", a.hits))
+	repr.WriteString(fmt.Sprintf("| %-20s | %-30v|\n", "Log File", a.logFile))
+	repr.WriteString(strings.Repeat("-", 56) + "\n")
 
-	return repr
+	return repr.String()
 }
 
 func ValidateArgs(values Arguments) {
+
+	// validating execution mode and path to config file.
 	if !values.s && !values.b {
-		panic("Please provide a mode for execution.")
+		log.Fatal("Please provide a mode for execution.")
 	} else if values.s && values.b {
-		panic("Please provide a single mode of execution.")
+		log.Fatal("Please provide a single mode of execution.")
 	} else if values.b && len(values.configFilePath) == 0 {
-		panic("Please provide a config for batch mode execution.")
+		log.Fatal("Please provide a config for batch mode execution.")
 	}
 
+	// checking for correct files and arguments
+	if len(values.body) > 0 && len(values.bodyFile) > 0 {
+		log.Fatal("Please provide only one arg for body.")
+	}
+
+	if len(values.bodyFile) > 0 && len(values.configFilePath) > 0 {
+		log.Fatal("Please provide either body-file or config-file")
+	}
+
+	// validating presence of args in singleton mode
 	if values.configFilePath == "" {
-		if len(values.body) > 0 && len(values.bodyFile) > 0 {
-			panic("Please provide only one arg for body.")
-		} else if values.method == "" {
-			panic("Please provide proper methods.")
+		if values.method == "" {
+			log.Fatal("Please provide proper methods.")
 		} else if values.base == "" {
-			panic("Please provide a base URL.")
+			log.Fatal("Please provide a base URL.")
 		}
 	}
 }
