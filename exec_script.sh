29d082b30235ca5eb31038048dc2d360786a0c79
Aryan <aryanmaurya1@outlook.com>
2021-05-22 20:36:31 +0530

    Written http request function.

--- exec_script.sh
+++ exec_script.sh
@@ -1,6 +1,7 @@
 go fmt *.go 
-go run *.go -s --body "{'name' : 'Aryan'}" \
+go run *.go -s \
+--body '{"name" : "Aryan"}' \
 --base https://api.writups.tech \
 --hits 10 --method GET,POST --ep /user \
---bodyfile ./config.json \
---file ./config.json
+--body-file ./config.json \
+--config-file ./config.json
