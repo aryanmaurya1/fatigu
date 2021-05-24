b9f8b6a9f0de4aa2dc5d12b7e6ad3869974356fe
Aryan <aryanmaura1@outlook.com>
2021-05-24 16:32:05 +0530

    Updated execution script.

--- exec_script.sh
+++ exec_script.sh
@@ -4,26 +4,26 @@ go build -o ../strainbot
 
 cd ..
 
+./strainbot -s -y \
+--body '{"name" : "Aryan"}' \
+--method POST \
+--base https://api.writups.tech \
+--ep /user \
+--headers '{"h1" : "v1", "h2" : "v2"}' \
+--log-file  logs.sb \
+--hits 100 \
+--hit-start 1 \
+--hit-stop 50 \
+--hit-step 10
+
 # ./strainbot -s \
-# --body '{"name" : "Aryan"}' \
-# --method POST \
-# --base https://api.writups.tech \
-# --ep /user \
+# --hits 100 \
+# --method GET \
+# --base https://jsonplaceholder.typicode.com \
+# --ep /todos/1 \
 # --headers '{"h1" : "v1", "h2" : "v2"}' \
 # --log-file  logs.sb \
 # --hits 100 \
 # --hit-start 10 \
-# --hit-stop 500 \
+# --hit-stop 1000 \
 # --hit-step 10
-
-./strainbot -s \
---hits 100 \
---method GET \
---base https://jsonplaceholder.typicode.com \
---ep /todos/1 \
---headers '{"h1" : "v1", "h2" : "v2"}' \
---log-file  logs.sb \
---hits 100 \
---hit-start 10 \
---hit-stop 500 \
---hit-step 10
