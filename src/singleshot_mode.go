3b7ad0acee38ac9a0232278ae487129938b69e7e
Aryan <aryanmaura1@outlook.com>
2021-05-25 11:18:54 +0530

    Now using channel to take metric from workers.

--- src/singleshot_mode.go
+++ src/singleshot_mode.go
@@ -1,28 +1,32 @@
 package main
 
 import (
-	"fmt"
 	"sync"
 )
 
 func singleshot(v RequestConfiguration) []Metric {
 
 	var holder *sync.WaitGroup = new(sync.WaitGroup)
+	var queue = make(chan Metric, 100)
 	var metrics []Metric = make([]Metric, 0, v.Hits)
-	fmt.Println(v)
+	// fmt.Println(v)
 	for i := int64(0); i < int64(v.Hits); i++ {
 
-		go func(holder *sync.WaitGroup, v RequestConfiguration, id int64) {
+		go func(holder *sync.WaitGroup, v RequestConfiguration, id int64, queue chan<- Metric) {
 
 			metric, _ := Hit(v.Method, v.BaseURL+v.Ep, string(v.Body), v.Headers)
 			metric.RoutineId = id
-			metrics = append(metrics, metric)
+			// metrics = append(metrics, metric)
+			queue <- metric
 
 			holder.Done()
 
-		}(holder, v, i)
+		}(holder, v, i, queue)
 		holder.Add(1)
 
+	}
+	for i := int64(0); i < int64(v.Hits); i++ {
+		metrics = append(metrics, <-queue)
 	}
 	holder.Wait()
 	return metrics
